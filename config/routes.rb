2ac7fdc026a50ad6e07dc89fa331bb253ddc3ffa
Yihuan Zhou <yihuan.zhou@gmail.com>
2012-06-14 20:27:39 -0400

    added sign in and sign out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	match '/auth/:provider/callback', to: 'sessions#create'
-  match '/signout', to: 'sessions#destroy', as: 'signout'
+  match '/signout', to: 'sessions#destroy', as: 'logout'
   match '/auth/usermanager', as: 'login'
 
   root to: 'home#index'
