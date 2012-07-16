152f39a7bdfcdc697ce66a78b77399b4bdb36a3d
Yihuan Zhou <yihuan.zhou@gmail.com>
2012-07-16 14:36:07 -0400

    made auth route explicit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 UserManagerClient::Engine.routes.draw do
 
-  match '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/usermanager/callback', to: 'sessions#create'
   match '/signout', to: 'sessions#destroy', as: 'logout'
   match '/auth/usermanager', as: 'login'
   match '/auth/redirect', to: 'sessions#redirect'
